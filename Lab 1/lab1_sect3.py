7334a0bdd9dbb449afe11b88a5be4f298d3de44d
Sidharth Shanmugam <ss2985@its.york.ac.uk>
2023-10-06 09:52:36 +0100

    lab 1 - part 1, section 3

--- Lab 1/lab1_sect3.py
+++ Lab 1/lab1_sect3.py
@@ -9,7 +9,20 @@ Created on Fri Oct  6 09:36:38 2023
 import cv2
 import numpy as np
 
-cap = cv2.VideoCapture(0)
+cap = cv2.VideoCapture(1)
 if not cap.isOpened():
     print("Cannot open camera")
     exit()
+    
+cap.set(cv2.CAP_PROP_FRAME_WIDTH, 1280)
+cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 720)
+
+cv2.namedWindow("Video Stream")
+success, img = cap.read()
+
+while success and cv2.waitKey(1) == -1:
+	cv2.imshow("Video Stream", img)
+	success, img = cap.read()
+
+cv2.destroyWindow("Video Stream")
+cap.release()
